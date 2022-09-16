017dbdc2f5a5ee3c6f142d661b25405107047738
Oleksii Kruhlyk <oleksii.kruhlyk@gmail.com>
2022-09-16 22:53:47 +0300

    Login page fix

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -14,15 +14,20 @@
                             <h3 class="font-semibold text-xl text-gray-800 leading-tight">{{ $todo->title }}</h3>
                             <p>{{ $todo->body }}</p>
                             <p class="mt-4 text-xs">Added at: {{ $todo->created_at->format('d/m/Y H:i:s') }}</p>
+                            @if($todo->isDone())
+                                <p class="mt-4 text-xs">Done at: {{ $todo->deleted_at->format('d/m/Y H:i:s') }}</p>
+                            @endif
                         </div>
-                        <div class="md:grid md:grid-cols-1 sm:px-2 lg:px-6 sm:my-2 lg:my-4">
-                            <div class="md:col-span-1">
-                                <a href="{{ route('todos.edit', ['todo' => $todo->getKey()]) }}"
-                                   class="inline-flex justify-center rounded-md border border-transparent bg-orange-400 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-orange-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">Edit</a>
-                                <a href="{{ route('todos.done', ['todo' => $todo->getKey()]) }}"
-                                   class="inline-flex justify-center rounded-md border border-transparent bg-green-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">Done</a>
+                        @if(! $todo->isDone())
+                            <div class="md:grid md:grid-cols-1 sm:px-2 lg:px-6 sm:my-2 lg:my-4">
+                                <div class="md:col-span-1">
+                                    <a href="{{ route('todos.edit', ['todo' => $todo->getKey()]) }}"
+                                       class="inline-flex justify-center rounded-md border border-transparent bg-orange-400 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-orange-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">Edit</a>
+                                    <a href="{{ route('todos.done', ['todo' => $todo->getKey()]) }}"
+                                       class="inline-flex justify-center rounded-md border border-transparent bg-green-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">Done</a>
+                                </div>
                             </div>
-                        </div>
+                        @endif
                     </div>
                 @endforeach
             @endif
