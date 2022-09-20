b46be6880f90e2210d4f4d84c9c7c4335ae09815
Oleksii Kruhlyk <oleksii.kruhlyk@gmail.com>
2022-09-20 18:54:02 +0300

    Create/Update todos form update

--- resources/views/todos/form.blade.php
+++ resources/views/todos/form.blade.php
@@ -7,7 +7,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg basis-1/4">
                 <form action="{{ route('todos.store') }}" method="POST">
-                    <input type="hidden" name="_token" value="{{ csrf_token() }}"/>
+                    @csrf
                     <div class="m:px-2 lg:px-6 sm:my-2 lg:my-4">
                         <label for="title" class="block text-sm font-medium text-gray-700">Title</label>
                         <div class="relative mt-1 rounded-md shadow-sm">
